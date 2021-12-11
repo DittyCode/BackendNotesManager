72eb52b1dd060ea98be7a7ac652a9fba84e6b232
Kamil <kam.nas21@wp.pl>
2021-12-11 11:57:49 +0100

    #7 Create get single note controller feature

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -13,7 +13,23 @@ const getAllNotes = async (req, res) => {
 			},
 		});
 	} catch (err) {
-		res.status(400).send({ message: 'Error', err });
+		res.status(400).send({ message: 'Error', status: 'failed', err });
+	}
+};
+
+const getSingleNote = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const note = await Note.findById(id);
+		res.status(200).send({
+			message: 'Get single note',
+			status: 'success',
+			data: {
+				note,
+			},
+		});
+	} catch (err) {
+		res.status(400).send({ message: 'Error', status: 'failed', err });
 	}
 };
 
@@ -28,11 +44,12 @@ const createNewNote = async (req, res) => {
 			},
 		});
 	} catch (err) {
-		res.status(400).send({ message: 'Error', err });
+		res.status(400).send({ message: 'Error', status: 'failed', err });
 	}
 };
 
 module.exports = {
 	getAllNotes,
 	createNewNote,
+	getSingleNote,
 };
--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -2,10 +2,13 @@ const express = require('express');
 const {
 	getAllNotes,
 	createNewNote,
+	getSingleNote,
 } = require('./../controllers/notesController');
 
 const router = express.Router();
 
 router.route('/').get(getAllNotes).post(createNewNote);
+
+router.route('/:id').get(getSingleNote);
 
 module.exports = router;
